50cea1285183243462c528520816036c0fe08d66
Debashish Paul <debashish333paul@gmail.com>
2019-05-06 11:41:16 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ class apps{
 		public function __construct(){
 			$this->links = mysqli_connect($this->hostname,$this->username,$this->password,$this->database);
 			if($this->links->connect_error){
-				echo "no connected";
+				echo "no connected and ";
 			}else{
-				echo "connected";
+				echo "connected and ";
 			}
 		}
 		
@@ -21,6 +21,55 @@ class apps{
 			if($insert){
 				throw new exception;
 			}
+		}
+	}
+
+
+
+
+
+if(isset($_POST['submit'])){
+		
+		
+		
+		$name = $_POST['name'];
+		$mobile = $_POST['mobile'];
+		$gender = $_POST['gender'];
+			
+		
+		
+		$error = 0;
+		$msg = "";
+		
+		if($name == ""){
+			$error = $error+1;
+			$user_error = "<span style='color:#f00'>* name required</span>";
+			$msg .= "<p style='color:#f00'>* name Required</p>";
+		}
+		if($mobile == ""){
+			$error = $error+1;
+			$msg .= "<p style='color:#f00'>* mobile Required</p>";
+		}
+		
+		
+		
+		if($error == 0){
+		
+			$insert = "INSERT INTO contact VALUES('', '$name', '$mobile', '$gender',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+			if($insert){
+				try{
+					$object = new apps;
+					$object->insert($insert);
+					echo "Not inserted";
+				}catch(exception $e){
+					echo "inserted";
+					
+				}
+			}
+		
+		
+		}else{
+			echo $msg;
 		}
 	}
 
@@ -35,24 +84,28 @@ class apps{
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+
+
+	 
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-sm-12">
-				<form>
+				<form action="" method="post">
 					<div class="content"><br><h3>contact save system</h3><br></div>
-					<div class="main_form">
+					
 					<input class="form-control" type="text" name="name" placeholder="type name here…"><br>
 					<input class="form-control" type="text" name="mobile" placeholder="type mobile number here…"><br>
-					<div class="form-group">
+					
 						<select id="inputState" name="gender" class="form-control">
 							<option selected>male</option>
 							<option>female</option>
 						</select>
-					</div><br>
-					<input type="file" name="image" class="form-control-file" id="exampleFormControlFile1"><br>
+					
+					<br>
 					<button type="submit" name="submit" class="btn btn-primary mb-2">Confirm identity</button>
-					</div>
-				</form>
+
+					</form>
+				
 			</div>
 		</div>
 	</div>
